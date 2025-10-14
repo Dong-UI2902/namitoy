a89d2d7cbd4da8412f8bce137ac99fb8bad9eee0
Dong-UI2902 <dongnv2902@gmail.com>
2025-10-14 13:12:16 +0700

    update link

--- src/config/Constain.ts
+++ src/config/Constain.ts
@@ -1,3 +1,3 @@
 export const FACEBOOK_LINK = "https://www.facebook.com/namitoyshop1";
 export const DISCORD_LINK = "https://discord.gg/caoxbanzai";
-export const GROUP_LINK = "https://www.facebook.com/groups/lanthu22"
+export const GROUP_LINK = "https://www.facebook.com/groups/2489686291424735"
